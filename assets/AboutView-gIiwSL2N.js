ba963d001fc29f23c81aaf8f4de6e5890e5a0782
Joseph van der Harst <josephvanderharst@gmail.com>
2026-08-19 10:33:15 -0400

    Added configuration for green and yellow ranges.

---
+++ assets/AboutView-gIiwSL2N.js
@@ -0,0 +1,1 @@
+import{c as e,m as t,t as n,u as r}from"./index-k7HXQCUV.js";var i={},a={class:`about`};function o(n,i){return t(),r(`div`,a,[...i[0]||=[e(`h1`,null,`This is an about page`,-1)]])}var s=n(i,[[`render`,o]]);export{s as default};
